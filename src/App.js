a8029008d8dc4af57e58cfe3debeac9388fc1449
Karaulovskin <karaulovskin@yandex.ru>
2020-05-08 16:11:29 +0300

    add Layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Layout from "./hoc/Layout/Layout";
 
 function App() {
     return (
         <div className="App">
-            <h1>UJIN</h1>
+            <Layout>
+                <h1>Ujin</h1>
+            </Layout>
         </div>
     );
 }
