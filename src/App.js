2d003783678b6da72f291525363373b81b9ddbae
Karaulovskin <karaulovskin@yandex.ru>
2020-05-08 16:29:07 +0300

    add instagram

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
         <div className="App">
             <Layout>
                 <h1>Ujin</h1>
+                <a href="https://www.instagram.com/ujin.camera/" target="_blank">instagram</a>
             </Layout>
         </div>
     );
