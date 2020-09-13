0231eb66b804e7579dcdf6cf2e7aa64b0e207de8
karaulovskin <karaulovskin@yandex.ru>
2020-09-13 13:42:28 +0300

    add video for main page

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react'
+import classes from './Main.module.css'
 
 export default class Main extends Component {
     render() {
         return (
-            <div>
+            <div className={classes.Main}>
                 <h1>8-Видео</h1>
             </div>
         )
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -3,17 +3,22 @@ import classes from './Layout.module.css'
 import Logo from '../../Navigation/Logo/Logo'
 import Menu from '../../Navigation/Menu/Menu'
 import Socials from '../../components/Socials/Socials'
+import MainVideo from '../../components/MainVideo/MainVideo'
+import {Route} from 'react-router-dom'
 
 class Layout extends Component {
     render() {
         return(
             <div className={classes.Layout}>
+                <Route path="/" exact render={MainVideo} />
                 <Logo />
                 <Menu />
                 <Socials />
 
                 <main>
-                    {this.props.children}
+                    <div className="container">
+                        {this.props.children}
+                    </div>
                 </main>
             </div>
         )
