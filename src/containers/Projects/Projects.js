fa69e3bcdc05bb910dd7783dc754b4b1c40e213f
Karaulovskin <karaulovskin@yandex.ru>
2020-09-17 18:55:47 +0300

    commit firebase

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -1,6 +1,14 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
 export default class Projects extends Component {
+
+    componentDidMount() {
+        axios.get('https://video-253d9.firebaseio.com/portfolio.json').then(response => {
+            console.log(response)
+        })
+    }
+
     render() {
         return (
             <div>
